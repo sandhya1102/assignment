853cd79e9e4ab18adef3a2453045328a9d224069
sandhya1102 <sandhay165@gmail.com>
2024-10-28 15:17:40 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-document.querySelectorAll(".img-container").forEach(container => {
-  const image = container.querySelector(".hover-image");
+const textcontent=document.querySelector(".hero-content");document.querySelectorAll(".img-container").forEach(container => {
 
   container.addEventListener("mousemove", (e) => {
       container.style.zIndex='10';
+      textcontent.style.opacity='0.3';
   });
 
   container.addEventListener("mouseleave", () => {
     container.style.zIndex='0'
+    textcontent.style.opacity='1';
   });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loadingScreen = document.querySelector(".loading-screen");
+
+  setTimeout(() => {
+      loadingScreen.style.display = "none";
+  }, 4000); 
 });
